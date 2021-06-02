ad343c44e9fb61234623cca4de4be4b03bcce943
hiepvo01 <vohi0101@luther.edu>
2021-06-02 17:21:16 -0500

    combine

--- backend/test.py
+++ backend/test.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import os
 
-print(os.listdir("../Original_Excel_Files"))
+combine = pd.DataFrame()
 for file in os.listdir("../Original_Excel_Files"):
-    print(file)
     df = pd.read_excel("../Original_Excel_Files/"+file)
-    print(df.head())
+    df = df[["LCC", "Yr.","Title"]]
+    print(file)
+    print(len(df.columns))
+    print(df.columns)
+    combine = combine.append(df)
+combine.to_csv("../goalData/original.csv", index=False)
+    
 
